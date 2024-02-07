dadadf7517d1ad13124cadedcd3adfe88b18aceb
LukasMystic <112644367+LukasMystic@users.noreply.github.com>
2024-02-07 22:12:40 +0700

    Create ProblemF.c

---
+++ JAWABAN/WEEK 2/ProblemF.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char input[100];
+    scanf("%[^\n]", input);
+
+    printf("#include <stdio.h>\n");
+    printf("int main() {\n");
+    printf("    printf(\"%%s\\n\",\"%s\");\n", input);
+    printf("    return 0;\n");
+    printf("}\n");
+
+    return 0;
+}
